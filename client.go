72f55748b4c728f6a0a028c4b338056e397dd9a8
Ariel Pierot <ariel.pierot@dcomp.ufs.br>
2019-03-22 02:16:59 +0000

    Mudanças no código

--- client.go
+++ client.go
@@ -1,11 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/ArielPierot/tmqp-api-client"
 )
 
 func main() {
 
-	tmqp.Handshake()
+	var comando int8
+
+	conn := tmqp.Handshake()
+
+	for {
+		fmt.Print("Informe o comando desejado: ")
+		fmt.Scanf("%d\n", &comando)
+		tmqp.SendPackage(conn, comando)
+	}
 
 }
